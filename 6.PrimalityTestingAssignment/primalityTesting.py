984840c63f814012886c1b88794f85fd08c97294
SwethaPraba <swetha.prabakaran@gmail.com>
2017-01-23 21:29:13 -0500

    primality code

--- 6.PrimalityTestingAssignment/primalityTesting.py
+++ 6.PrimalityTestingAssignment/primalityTesting.py
@@ -1,6 +1,5 @@
-#https://www.youtube.com/watch?t=2&v=W4UgfGu7cbM <- champs of change link don't lose this
 from sympy import *
-from random import randint
+from numb
 def methodOne(n):
 	prime = "PRIME"
 	composite = "COMPOSITE"
@@ -18,11 +17,10 @@ def methodOne(n):
 			print("%s : YES"%number)
 			number+=1
 	return decision
-
 def millerRabinPrimality(n,k):
 	prime = "Probably PRIME"
 	composite = "COMPOSITE"
-	decision = prime
+	decision = composite
 	###write code here
 	if n == 3:
 		decision = prime
@@ -40,40 +38,22 @@ def millerRabinPrimality(n,k):
 		break
 	else:
 		variable = 2 ** n-1 #this is obviously wrong
-		d = 0 #because I don't know what d is yet
-		r = 10 #don't know what r is though ngl
 		for i in k:
 			#the loop thing
-			a = randint(2,n-2)
-			x = (a ** d ) % n
-			if x == 1 or x == (n-1):
-				#continue with the algorithm
-				for thing in xrange(0,r-1):
-					x = (x**2) % n
-					if x == 1:
-						decision = composite
-					else if x = (n-1):
-						pass
-					else:
-						decision = prime #i think??
-						#idk what I'm doing
 
-			else:
-				decision = prime #i think? or is this composite? What is actually going on??
 		'''
 		write n − 1 as 2^r(·d, not in the variable) with d odd by factoring powers of 2 from n − 1
 		WitnessLoop: repeat k times:
   		pick a random integer a in the range [2, n − 2]
    		x ← ad mod n
    		if x = 1 or x = n − 1 then
-    		continue WitnessLoop
+    	continue WitnessLoop
    		repeat r − 1 times:
-      		x ← x squared mod n
-      		if x = 1 then
-        		return composite
-      		if x = n − 1 then
-        		continue WitnessLoop
-   		
+      	x ← x2 mod n
+      	if x = 1 then
+        return composite
+      	if x = n − 1 then
+        continue WitnessLoop
    		return composite
 		return probably prime
 		'''
