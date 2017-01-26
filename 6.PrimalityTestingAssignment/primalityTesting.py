c39a1257302d11033a37245af7aefe994e456fa8
SwethaPraba <swetha.prabakaran@gmail.com>
2017-01-25 19:19:08 -0500

    miller rabin primality test coding

--- 6.PrimalityTestingAssignment/primalityTesting.py
+++ 6.PrimalityTestingAssignment/primalityTesting.py
@@ -18,29 +18,52 @@ def methodOne(n):
 			number+=1
 	return decision
 def millerRabinPrimality(n,k):
-	prime = "Probably PRIME"
-	composite = "COMPOSITE"
-	decision = composite
+	prime = "Probably PRIME" #true
+	composite = "COMPOSITE" #false
 	###write code here
 	if n == 3:
-		decision = prime
-		break
+		return True
 	else if n == 2:
-		decision = composite
-		break
+		return False
 	else if n == 1:
-		decision = prime
-		break
+		return True
 	else if n <= 1:
-		decision = "not doable. must be greater than 3"
+		return False
 	else if (n % 2) == 0:
-		decision = composite #number is even we are done
-		break
+		return False #number is even we are done
 	else:
-		variable = 2 ** n-1 #this is obviously wrong
-		for i in k:
+		r,d = factor(n-1) #must start with an even number!
+		for i in range(k):
 			#the loop thing
+			a = random.randrange(2, n-1)
+			x = pow(a,d,n)
+			if x==1 or x== (n-1):
+				continue
+			done = False
+			while r > 0 and not done:
+				x = pow(x,2,n)
+				if x ==1:
+					return False
+				if x== n-1:
+					done = True
+				r = r-1
+			if not done:
+				return False
+	return True
 
+def factor(n):
+	#r is the number of steps
+	#d is that odd number
+	r = 0
+	d = n
+	if d % 2 == 0:
+		r = r+1
+		d = d//2
+	else:
+		r = r
+		d = d
+		return r, d
+	return r, d
 		'''
 		write n − 1 as 2^r(·d, not in the variable) with d odd by factoring powers of 2 from n − 1
 		WitnessLoop: repeat k times:
